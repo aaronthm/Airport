7c92dac3e7ed4eb170230b31023a0c043decc99a
Aaron Miyasato <aaronthm@uw.edu>
2018-11-15 18:36:04 -0800

    Cleaned Data? I think

--- statistical_analysis.R
+++ statistical_analysis.R
@@ -1,5 +1,12 @@
-setwd("~/Desktop/INFO370/Assignment/Final_Project")
+setwd("~/Desktop/INFO370/Final_Project")
 library(corrplot)
+library(dplyr)
 flight_delay <- read.csv("airline_delay_causes.csv")
-keep
-{year, month,arr_flight, arr_delay15, carrier_ct, weather_ct, has_ct, late_aircraft_ct, arr_delay }
+head(flight_delay)
+
+flight_delay = select(flight_delay, "year", "X.month", "carrier", "carrier_name","airport", "airport_name", "arr_flights", "arr_del15", "carrier_ct",
+                      "X.weather_ct","nas_ct", "late_aircraft_ct", "arr_diverted")
+
+by_flight = flight_delay %>% group_by(airport, year, month)
+
+summary(by_flight)
