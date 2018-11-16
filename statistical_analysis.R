738176996ba133a38fa040572bbdfe5df793167f
Aaron Miyasato <aaronthm@uw.edu>
2018-11-15 17:19:38 -0800

    First Commit

---
+++ statistical_analysis.R
@@ -0,0 +1,5 @@
+setwd("~/Desktop/INFO370/Assignment/Final_Project")
+library(corrplot)
+flight_delay <- read.csv("airline_delay_causes.csv")
+keep
+{year, month,arr_flight, arr_delay15, carrier_ct, weather_ct, has_ct, late_aircraft_ct, arr_delay }
